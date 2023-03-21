49cc8e6794d8a84a79eb60ec2051f55f215cf83c
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2023-03-21 17:01:17 +0530

    added more changes to server

--- CreateServer/Server.js
+++ CreateServer/Server.js
@@ -1,9 +1,14 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log("request made");
-});
+  console.log(req.url, req.method);
 
-server.listen(3000, "localhost", () => {
-  console.log("listening for requests on port 3000");
+  res.setHeader("Content-Type", "text/html");
+
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      comsole.log(err);
+    }
+  });
 });
